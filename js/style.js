ce8de9fd7fb1b8397e2aaa7015937985303f853f
misteakes <42952258+misteakes@users.noreply.github.com>
2019-02-15 01:01:08 -0500

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,43 @@
+for(var i=0; i<5; i++) {
+
+	var shape = document.createElement('div');
+	shape.classList.add('envelopes');
+	var container = document.querySelector('.content');
+	container.append(shape);
+
+}
+
+shape.addEventListener('mouseover', function(event){
+    shape.classList.add('shape-interaction');
+  });
+
+  var container = document.querySelector('.content');
+
+  container.append(shape);
+
+var shapes = document.querySelectorAll('.shape');
+
+shapes.forEach(function(shape, i){
+  shape.addEventListener('mouseover', function(event){
+
+    if (shape.classList.contains('shape-interaction')) {
+      shape.classList.remove('shape-interaction');
+    } else {
+      shape.classList.add('shape-interaction');
+    }
+    
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
